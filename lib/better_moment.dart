a0d49375995a7af7d3c40555fbd8dae32d8c0145
Lucas Vienna <lmvienna@outlook.com>
2018-11-12 10:48:49 +0100

    Added locale exports

--- lib/better_moment.dart
+++ lib/better_moment.dart
@@ -1,3 +1,8 @@
 library better_moment;
 
 export 'src/moment.dart';
+export 'src/locale/locale.dart';
+export 'src/locale/locales/de.dart';
+export 'src/locale/locales/en.dart';
+export 'src/locale/locales/es.dart';
+export 'src/locale/locales/pt.dart';
